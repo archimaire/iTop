0ad4142a98a969d316321e24c470760bf88dbef0
Pierre Goiffon <pierre.goiffon@combodo.com>
2022-02-04 17:01:33 +0100

    Add button in application upgrade screen

--- datamodels/2.x/itop-core-update/dictionaries/en.dict.itop-core-update.php
+++ datamodels/2.x/itop-core-update/dictionaries/en.dict.itop-core-update.php
@@ -54,6 +54,7 @@ Dict::Add('EN US', 'English', 'English', array(
 
     'iTopUpdate:UI:Status' => 'Status',
     'iTopUpdate:UI:Action' => 'Update',
+    'iTopUpdate:UI:Setup' => 'iTop Setup',
     'iTopUpdate:UI:History' => 'Versions History',
     'iTopUpdate:UI:Progress' => 'Progress of the upgrade',
 
@@ -76,6 +77,9 @@ Dict::Add('EN US', 'English', 'English', array(
 	'iTopUpdate:UI:CanCoreUpdate:No' => 'Application cannot be updated: %1$s',
 	'iTopUpdate:UI:CanCoreUpdate:Warning' => 'Warning: application update can fail: %1$s',
 	'iTopUpdate:UI:CannotUpdateUseSetup' => 'You must use the <a href="%1$s">setup</a> to update the application.<br />Some modified files were detected, a partial update cannot be executed.',
+
+	'iTopUpdate:UI:SetupLaunch' => 'Launch iTop Setup',
+	'iTopUpdate:UI:SetupLaunchConfirm' => 'This will launch iTop setup, are you sure ?',
 
 	// Setup Messages
     'iTopUpdate:UI:SetupMessage:Ready' => 'Ready to start',
