06f192bccbbb88868ebad322b2755cb23ad91aa0
Pierre Goiffon <pierre.goiffon@combodo.com>
2022-03-03 12:36:39 +0100

    :speech_balloon: Fix english typography rule
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>

--- datamodels/2.x/itop-core-update/dictionaries/en.dict.itop-core-update.php
+++ datamodels/2.x/itop-core-update/dictionaries/en.dict.itop-core-update.php
@@ -79,7 +79,7 @@ Dict::Add('EN US', 'English', 'English', array(
 	'iTopUpdate:UI:CannotUpdateUseSetup' => 'You must use the <a href="%1$s">setup</a> to update the application.<br />Some modified files were detected, a partial update cannot be executed.',
 
 	'iTopUpdate:UI:SetupLaunch' => 'Launch '.ITOP_APPLICATION_SHORT.' Setup',
-	'iTopUpdate:UI:SetupLaunchConfirm' => 'This will launch '.ITOP_APPLICATION_SHORT.' setup, are you sure ?',
+	'iTopUpdate:UI:SetupLaunchConfirm' => 'This will launch '.ITOP_APPLICATION_SHORT.' setup, are you sure?',
 
 	// Setup Messages
     'iTopUpdate:UI:SetupMessage:Ready' => 'Ready to start',
