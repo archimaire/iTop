7620e0b77ec608bb6a73b83b0c7a201afdd780b6
Pierre Goiffon <pierre.goiffon@combodo.com>
2022-03-04 10:49:18 +0100

    Fix autoload and iTop base test classes Easy to see : just launch `php unittestautoload.php` (or see fatal errors in IDE)

--- test/unittestautoload.php
+++ test/unittestautoload.php
@@ -1,7 +1,5 @@
 <?php
+include('./vendor/autoload.php');
 
-@include('ItopTestCase.php');
-@include('ItopDataTestCase.php');
-
-
-@include('./vendor/autoload.php');
+include('ItopTestCase.php');
+include('ItopDataTestCase.php');
