9aaa51078effcacd0a5e6c74ef3b3f0dd6760bae
Pierre Goiffon <pierre.goiffon@combodo.com>
2022-03-04 11:15:05 +0100

    Fix setUp and teardDown methods signatures "Return type declaration must be compatible with parent" See https://phpunit.de/announcements/phpunit-8.html "Return Type of Template Methods"

--- test/core/DBObjectTest.php
+++ test/core/DBObjectTest.php
@@ -39,7 +39,7 @@ class DBObjectTest extends ItopDataTestCase
 {
 	const CREATE_TEST_ORG = true;
 
-	protected function setUp()
+	protected function setUp(): void
 	{
 		parent::setUp();
 		require_once(APPROOT.'core/coreexception.class.inc.php');
